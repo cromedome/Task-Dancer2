1c551b6ae36bc8bedef949dea14eda936816471d
Jason A. Crome <jason@crome-plated.com>
2021-03-15 00:22:47 -0400

    WIP: Update modules in Dancer2 ecosystem.
    From here, I need to test installing these and see what breaks and what
    doesn't, then classify accordingly.

--- lib/Task/Dancer2.pm
+++ lib/Task/Dancer2.pm
@@ -35,9 +35,13 @@ tell us if any module that was temporarily disabled is working again.
 
 =over 4
 
+=item L<Dancer2::Template::Alloy>
+
 =item L<Dancer2::Template::Caribou> (disabled)
 
 =item L<Dancer2::Template::Haml>
+
+=item L<Dancer2::Template::Handlebars>
 
 =item L<Dancer2::Template::HTCompiled>
 
@@ -45,11 +49,13 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Template::MojoTemplate>
 
+=item L<Dancer2::Template::Mustache>
+
 =item L<Dancer2::Template::TemplateFlute>
 
 =item L<Dancer2::Template::TextTemplate>
 
-=item L<Dancer2::Template::Xslate> (disabled)
+=item L<Dancer2::Template::Xslate>
 
 =back
 
@@ -59,7 +65,19 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Logger::Console::Colored>
 
+=item L<Dancer2::Logger::Fluent>
+
+=item L<Dancer2::Logger::File::Radis>
+
 =item L<Dancer2::Logger::File::RotateLogs>
+
+=item L<Dancer2::Logger::Log4perl>
+
+=item L<Dancer2::Logger::LogAny>
+
+=item L<Dancer2::Logger::LogReport>
+
+=item L<Dancer2::Logger::Multiplex>
 
 =item L<Dancer2::Logger::Syslog>
 
@@ -71,6 +89,8 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Serializer::CBOR>
 
+=item L<Dancer2::Serializer::XML>
+
 =back
 
 =head2 Session Engines
@@ -78,6 +98,8 @@ tell us if any module that was temporarily disabled is working again.
 =over 4
 
 =item L<Dancer2::Session::CGISession>
+
+=item L<Dancer2::Session::CHI>
 
 =item L<Dancer2::Session::Cookie>
 
@@ -107,21 +129,41 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::AppRole::Helper>
 
+=item L<Dancer2::Plugin::Argon2>
+
 =item L<Dancer2::Plugin::Articulate>
+
+=item L<Dancer2::Plugin::Auth::ActiveDirectory>
+
+=item L<Dancer2::Plugin::Auth::CAS>
 
 =item L<Dancer2::Plugin::Auth::Extensible>
 
+=item L<Dancer2::Plugin::Auth::Extensible::Provider::ActiveDirectory>
+
+=item L<Dancer2::Plugin::Auth::Extensible::Provider::Database>
+
 =item L<Dancer2::Plugin::Auth::Extensible::Provider::DBIC>
 
+=item L<Dancer2::Plugin::Auth::Extensible::Provider::IMAP>
+
+=item L<Dancer2::Plugin::Auth::Extensible::Provider::LDAP>
+
 =item L<Dancer2::Plugin::Auth::Extensible::Provider::Usergroup>
+
+=item L<Dancer2::Plugin::Auth::Extensible::Rights>
 
 =item L<Dancer2::Plugin::Auth::HTTP::Basic::DWIW>
 
 =item L<Dancer2::Plugin::Auth::OAuth>
 
+=item L<Dancer2::Plugin::Auth::SAFE>
+
 =item L<Dancer2::Plugin::Auth::Tiny>
 
 =item L<Dancer2::Plugin::Auth::YARBAC> (disabled)
+
+=item L<Dancer2::Plugin::BeforeRoute>
 
 =item L<Dancer2::Plugin::BrowserDetect>
 
@@ -129,9 +171,17 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::Captcha> 
 
+=item L<Dancer2::Plugin::Cart> 
+
 =item L<Dancer2::Plugin::Chain>
 
 =item L<Dancer2::Plugin::ConditionalCaching>
+
+=item L<Dancer2::Plugin::ConditionalRequest>
+
+=item L<Dancer2::Plugin::ContentNegotiation>
+
+=item L<Dancer2::Plugin::CSRF>
 
 =item L<Dancer2::Plugin::Database>
 
@@ -139,7 +189,13 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::DBIC>
 
+=item L<Dancer2::Plugin::DBIx::Class>
+
+=item L<Dancer2::Plugin::DebugDump>
+
 =item L<Dancer2::Plugin::Deferred>
+
+=item L<Dancer2::Plugin::EditFile>
 
 =item L<Dancer2::Plugin::ElasticSearch> (disabled)
 
@@ -147,17 +203,41 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::Emailesque>
 
+=item L<Dancer2::Plugin::EncryptID>
+
+=item L<Dancer2::Plugin::Etcd>
+
 =item L<Dancer2::Plugin::Feed>
 
+=item L<Dancer2::Plugin::Flash>
+
+=item L<Dancer2::Plugin::FlashNote>
+
+=item L<Dancer2::Plugin::FontSubset>
+
+=item L<Dancer2::Plugin::Github::Webhook>
+
 =item L<Dancer2::Plugin::GoogleAnalytics>
+
+=item L<Dancer2::Plugin::GraphQL>
 
 =item L<Dancer2::Plugin::Growler>
 
 =item L<Dancer2::Plugin::HTTP::Auth::Extensible> (disabled)
 
+=item L<Dancer2::Plugin::HTTP::Bundle>
+
+=item L<Dancer2::Plugin::HTTP::Caching>
+
+=item L<Dancer2::Plugin::Interchange6>
+
+=item L<Dancer2::Plugin::JSManager>
+
 =item L<Dancer2::Plugin::JWT>
 
 =item L<Dancer2::Plugin::Locale> (disabled)
+
+=item L<Dancer2::Plugin::Locale::Meta>
 
 =item L<Dancer2::Plugin::Locale::Wolowitz> (disabled)
 
@@ -165,21 +245,49 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::LogReport>
 
+=item L<Dancer2::Plugin::Map::Tube>
+
+=item L<Dancer2::Plugin::MarkdownFilesToHTML>
+
+=item L<Dancer2::Plugin::Menu>
+
+=item L<Dancer2::Plugin::Minify>
+
+=item L<Dancer2::Plugin::Minion>
+
+=item L<Dancer2::Plugin::MobileDevice>
+
 =item L<Dancer2::Plugin::Model>
 
 =item L<Dancer2::Plugin::Multilang>
 
 =item L<Dancer2::Plugin::Negotiate>
 
+=item L<Dancer2::Plugin::OAuth2::Server>
+
+=item L<Dancer2::Plugin::OpenAPIRoutes>
+
+=item L<Dancer2::Plugin::PageHistory>
+
+=item L<Dancer2::Plugin::Paginator>
+
 =item L<Dancer2::Plugin::ParamKeywords>
+
+=item L<Dancer2::Plugin::ParamTypes>
 
 =item L<Dancer2::Plugin::Passphrase>
 
 =item L<Dancer2::Plugin::Path::Class>
 
+=item L<Dancer2::Plugin::Pg>
+
+=item L<Dancer2::Plugin::ProbabilityRoute>
+
 =item L<Dancer2::Plugin::ProgressStatus>
 
 =item L<Dancer2::Plugin::Queue> 
+
+=item L<Dancer2::Plugin::Queue::IronMQ> 
 
 =item L<Dancer2::Plugin::Queue::MongoDB> 
 
@@ -195,11 +303,49 @@ tell us if any module that was temporarily disabled is working again.
 
 =item L<Dancer2::Plugin::RoutePodCoverage>
 
+=item L<Dancer2::Plugin::Routing>
+
+=item L<Dancer2::Plugin::SessionDatabase>
+
+=item L<Dancer2::Plugin::Showterm>
+
+=item L<Dancer2::Plugin::Shutdown>
+
+=item L<Dancer2::Plugin::Shutdown::Redis>
+
 =item L<Dancer2::Plugin::Sixpack>
+
+=item L<Dancer2::Plugin::SPID>
+
+=item L<Dancer2::Plugin::Swagger2>
+
+=item L<Dancer2::Plugin::SyntaxHighlight::Perl>
 
 =item L<Dancer2::Plugin::Syntax::GetPost>
 
+=item L<Dancer2::Plugin::Tail>
+
 =item L<Dancer2::Plugin::UnicodeNormalize>
+
+=item L<Dancer2::Plugin::WebService>
+
+=item L<Dancer2::Plugin::WebSocket>
+
+=back
+
+=head2 Other
+
+These modules don't quite fit in another category.
+
+=over 4
+
+=item L<Dancer2::Debugger>
+
+=item L<Dancer2::UserAdmin>
+
+=item L<Plack::Debugger::Panel::Dancer2::Version>
+
+=item L<SOAP::WSDL::Server::Dancer2>
 
 =back
 
